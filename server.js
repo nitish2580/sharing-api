a2abac065552f01346ad545e1cabc3ffd6311462
nitish2580 <email>
2022-09-16 20:47:24 +0530

    changer in server

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const app=express();
 const PORT =process.env.PORT||3000;
 const cors=require('cors')
 //Cors
-const corsOptions ={
-    origin: process.env.ALLOWEd_CLIENTS.split(',')
-}
-app.use(cors(corsOptions))
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(","),
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+};
+app.use(cors(corsOptions));
 //Template Engine
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
