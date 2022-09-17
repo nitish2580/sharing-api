d92020619ce687e597ae37e329126e0d278d35ba
nitish2580 <email>
2022-09-17 06:20:51 +0530

    changes

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const express=require("express");
 const mongoose = require("./Config/db");
 const path=require('path');
 const app=express();
-const cors=require('cors')
-//Cors
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+// Cors
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(","),
 };
+
+
 app.use(cors(corsOptions));
 //Template Engine
 app.set('views',path.join(__dirname,'/views'));
@@ -20,10 +23,4 @@ app.use('/files',require('./Route/show'));
 app.use('/files/download',require('./Route/download'))
 
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
